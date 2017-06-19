a3d9e4d45335f461a7625dcc4fdafe6055522daa
Douglass Hooker <DouglassHooker@gmail.com>
2017-06-19 13:34:51 -0500

    Added more Array exercises. Consolidated HTML files

---
+++ public/js/mad_libs.js
@@ -0,0 +1,18 @@
+(function() {
+	"use strict";
+	
+		var name = prompt("What is your name?");
+		console.log(name);
+	
+	
+		var options = [];
+	
+		function madLib() {
+			options[0] = prompt("1. Please enter a verb in present tense");
+			options[1] = prompt("2. Please enter a noun");
+			
+		}
+	
+	
+	madLib();
+}
---
+++ public/js/temperature.js
@@ -0,0 +1,14 @@
+/* TODO: Make array with variables F, C, and K. Take input and tempScale, 
+do math, and throw out scaled temperatures */
+
+
+
+(function() {
+"use strict";
+
+
+var tempScale = ["Fahrenheit", "Celsius", "Kelvin"]
+
+
+
+});
