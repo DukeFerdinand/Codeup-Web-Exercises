4c55f7b974f598a2b6c36bb663d5e2942aa6c048
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-07 15:55:30 -0500

    Added slicker animations to the About Me page

--- public/js/my-site.js
+++ public/js/my-site.js
@@ -1,19 +1,19 @@
 "use strict";
 //Adds dropdown function to upper menu bar
 $('#dropdown-button').click(function(){
-    $('#mobile-hidden').fadeToggle(200);
+    $('#mobile-hidden').slideToggle(100);
 });
 
 
 $('#hidden1').click(function(){
-    $('#mobile-hidden').fadeToggle(200);
+    $('#mobile-hidden').slideToggle(100);
     $('#sidebar1').animate({
+        'margin-left': '200px',
+        'border-radius': '10px',
         width: '800px',
         height: '800px',
         'margin-top': '50px'
-    }, 800).animate({
-        'margin-left': '200px',
-        'border-radius': '10px',
+    }, 500).animate({
         'box-shadow': '3px 3px 3px rgba(0,0,0,0.5)'
     }, {
         duration: 250,
@@ -21,7 +21,7 @@ $('#hidden1').click(function(){
             $('.hidden-profile').animate({
                 opacity: 1
             }, {
-                duration: 1000
+                duration: 500
             })
         }
     })
