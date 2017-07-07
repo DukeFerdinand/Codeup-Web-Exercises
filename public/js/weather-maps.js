0611f69b13ed328446d10c804d3b102c7dd9d852
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-07 13:59:19 -0500

    Finished the Weather Page, added click functionality to the map for selectiong city to get weather

--- public/js/weather-maps.js
+++ public/js/weather-maps.js
@@ -1,33 +1,65 @@
 'use strict';
 
-var mapElement = document.getElementById('map');
+// var mapElement = document.getElementById('map');
 
-var mapOptions = {
+// var mapOptions = {
 
-	//zoom level
+// 	//zoom level
 
-	zoom: 10,
+// 	zoom: 10,
 
-	// This sets the center of the map at our location
-     center: {
-                lat:  29.426791,
-                lng: -98.497888
-            },
-    mapTypeId: google.maps.MapTypeId.HYBRID
-}
+// 	// This sets the center of the map at our location
+//      center: {
+//                 lat:  29.426791,
+//                 lng: -98.497888
+//             },
+//     mapTypeId: google.maps.MapTypeId.HYBRID
+// }
 
-var map = new google.maps.Map(mapElement, mapOptions);
+// var map = new google.maps.Map(mapElement, mapOptions);
 
-google.maps.event.addListener(mapElement, 'click', function(event) {
-   placeMarker(event.latLng);
-});
 
-function placeMarker(location) {
-    var marker = new google.maps.Marker({
-        position: location, 
-        map: map
-    });
-}
+// //Attach click event handler to the map.
+// 	var marker;
+// 	var markerCount = 0;
+// google.maps.event.addListener(map, 'click', function (e) {
+//     //Determine the location where the user has clicked.
+//     var location = e.latLng;
+//     //Create a marker and placed it on the map.
+//     if (markerCount >= 1) {
+//     	marker.setMap(null);
+    	
+//     }
+
+//     marker = new google.maps.Marker({
+//         position: location,
+//         map: map
+//     });
+//     markerCount ++;
+//     console.log(markerCount);
+//     //Attach click event handler to the marker.
+//     google.maps.event.addListener(marker, "click", function (e) {
+//         var infoWindow = new google.maps.InfoWindow({
+//             content: 'Latitude: ' + location.lat() + '<br />Longitude: ' + location.lng()
+//         });
+//         infoWindow.open(map, marker);
+//     });
+// });
+
+
+
+
+
+// google.maps.event.addListener(mapElement, 'click', function(event) {
+//    placeMarker(event.latLng);
+// });
+
+// function placeMarker(location) {
+//     var marker = new google.maps.Marker({
+//         position: location, 
+//         map: map
+//     });
+// }
 
 // mapElement.addEventListener('click', function(e){
 // 	placeMarker(e.latLng, mapElement);
--- public/js/weather.js
+++ public/js/weather.js
@@ -1,70 +1,129 @@
 "use strict";
+var Lat;
+var Lon;
+getWeather(29.424349, -98.491142);
 
-// console.log('Hello World!')
-$.ajax("http://api.openweathermap.org/data/2.5/forecast/daily", {
+var mapElement = document.getElementById('map');
 
-    data: {
-        APPID: "1671273d8fb3358671b09111cf13ae9a",
-        q:     "San Antonio, TX",
-        cnt:   "7",
-        units: "imperial"
+var mapOptions = {
+
+	//zoom level
+
+	zoom: 10,
+
+	// This sets the center of the map at our location
+     center: {
+                lat:  29.424349,
+                lng: -98.491142
+            },
+    mapTypeId: google.maps.MapTypeId.HYBRID
+}
+
+var map = new google.maps.Map(mapElement, mapOptions);
+
+
+//Attach click event handler to the map.
+	var marker;
+	var markerCount = 0;
+google.maps.event.addListener(map, 'click', function (e) {
+    //Determine the location where the user has clicked.
+    var location = e.latLng;
+    //Create a marker and placed it on the map.
+    if (markerCount >= 1) {
+    	marker.setMap(null);
+    	
     }
 
-}).done(function(data){
-	//Using jQuery to sift through the API and find and display goodies relevent to the display
-		console.log(data)
-		var today;
-		var date = new Date();
-		
+    marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+    markerCount ++;
+    console.log(markerCount);
+    //Attach click event handler to the marker.
+    google.maps.event.addListener(marker, "click", function (e) {
+        var infoWindow = new google.maps.InfoWindow({
+            content: 'Latitude: ' + location.lat() + '<br />Longitude: ' + location.lng()
+        });
+        infoWindow.open(map, marker);
+    });
+    console.log(location.lat());
+    console.log(location.lng());
+        Lat = location.lat();
+        Lon = location.lng();
+        getWeather(Lat, Lon);
+});
 
-	for (var i = 0; i < data.list.length; i++){
 
-		console.log(data.list[i].temp.max);
+// console.log('Hello World!')
+	function getWeather (Lat, Lon){
+		$.ajax("http://api.openweathermap.org/data/2.5/forecast/daily", {
 
-		$('#city').html(data.city.name);
+	    data: {
+	        APPID: "1671273d8fb3358671b09111cf13ae9a",
+	        lat: Lat, 
+	        lon: Lon, 
+	        cnt:   "7",
+	        units: "imperial"
+	    }
 
-		for (var day = 0; day <= 7; day ++){
+	}).done(function(data){
+		//Using jQuery to sift through the API and find and display goodies relevent to the display
+			// console.log(data)
+			var today;
+			var date = new Date();
+			
 
-			today = date.getDay();
-			today = today + i;
-			today = (today % 7);
-			console.log(today);
-		switch (today) {
-			case 1:
-				today = "Monday";
-				break;
-			case 2:
-				today = "Tuesday";
-				break;
-			case 3:
-				today = "Wednesday";
-				break;
-			case 4:
-				today = "Thursday";
-				break;
-			case 5:
-				today = "Friday";
-				break;
-			case 6:
-				today = "Saturday";
-				break;
-			case 0:
-				today = "Sunday";
-				break;
-			default:
-				today = 'Error';
-				break;
+		for (var i = 0; i < data.list.length; i++){
+
+			// console.log(data.list[i].temp.max);
+
+			$('#city').html("Current City: " + data.city.name);
+
+			for (var day = 0; day <= 7; day ++){
+
+				today = date.getDay();
+				today = today + i;
+				today = (today % 7);
+				// console.log(today);
+			switch (today) {
+				case 1:
+					today = "Monday";
+					break;
+				case 2:
+					today = "Tuesday";
+					break;
+				case 3:
+					today = "Wednesday";
+					break;
+				case 4:
+					today = "Thursday";
+					break;
+				case 5:
+					today = "Friday";
+					break;
+				case 6:
+					today = "Saturday";
+					break;
+				case 0:
+					today = "Sunday";
+					break;
+				default:
+					today = 'Error';
+					break;
+
+				}
 			}
-		}
-		$('#date' + (i + 1)).append(today);
-		$('#temp' + (i + 1)).append("<h3>" + data.list[i].temp.max + '/' + data.list[i].temp.min + '℉ </h3>');
-		$('#icon' +(i + 1)).append("<img src='http://openweathermap.org/img/w/" + data.list[i].weather[0].icon +".png'><br>");
-		$('#cloud' + (i + 1)).append("<strong>" + data.list[i].weather[0].main.toLowerCase() + ": </strong>" + data.list[i].weather[0].description + '<br>');
-		$('#humidity' + (i + 1)).append("<strong>humidity: </strong>" + data.list[i].humidity + '<br>');
-		$('#wind' +(i + 1)).append("<strong>wind: </strong>" + data.list[i].speed + ' mph<br>');
-		$('#pressure' + (i + 1)).append("<strong>pressure: </strong>" + data.list[i].pressure + '<br>');
+			$('#date' + (i + 1)).text(today);
+			$('#temp' + (i + 1)).html("<h3>" + data.list[i].temp.max + '/' + data.list[i].temp.min + '℉ </h3>');
+			$('#icon' +(i + 1)).html("<img src='http://openweathermap.org/img/w/" + data.list[i].weather[0].icon +".png'><br>");
+			$('#cloud' + (i + 1)).html("<strong>" + data.list[i].weather[0].main.toLowerCase() + ": </strong>" + data.list[i].weather[0].description + '<br>');
+			$('#humidity' + (i + 1)).html("<strong>humidity: </strong>" + data.list[i].humidity + '<br>');
+			$('#wind' +(i + 1)).html("<strong>wind: </strong>" + data.list[i].speed + ' mph<br>');
+			$('#pressure' + (i + 1)).html("<strong>pressure: </strong>" + data.list[i].pressure + '<br>');
 
-		}
+			}
 
 
-})
+	})
+}
