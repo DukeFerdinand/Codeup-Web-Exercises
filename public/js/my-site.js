0f4aa5dbffdce6dd530ed70b43c0dfa2a4588377
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-06 20:46:37 -0500

    Added rough mobile responsive design to my-site.html, going to test on real phone

--- public/js/my-site.js
+++ public/js/my-site.js
@@ -2,14 +2,15 @@
 
 $('button').click(function(){
     $('#sidebar1').animate({
-        width: '200px',
-        height: '200px'
+        width: '400px',
+        height: '400px',
+        'margin-top': '50px'
     }, 1000).animate({
         'margin-left': '200px',
         'border-radius': '10px',
         'box-shadow': '3px 3px 3px rgba(0,0,0,0.5)'
     }, {
-        duration: 750,
+        duration: 500,
         complete: function(){
             $('.hidden-profile').animate({
                 opacity: 1
