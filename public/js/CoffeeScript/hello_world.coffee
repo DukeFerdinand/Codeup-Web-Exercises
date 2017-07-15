8c84e71d26af4e744fa57da9b9b2e6d3ca09aaef
Douglass Hooker <DouglassHooker@gmail.com>
2017-07-15 15:50:12 -0500

    Fixed calculator functionality and CSS, moved many things to personal site

--- public/js/CoffeeScript/hello_world.coffee
+++ public/js/CoffeeScript/hello_world.coffee
@@ -1,1 +1,8 @@
 console.log "Hello World"
+name = 'me'
+
+favorites =
+	food: ["pizza", "pasta"]
+	codingLanguages:["Sass", "CoffeeScript", "CSS"]
+
+console.log favorites
--- public/js/CoffeeScript/hello_world.js
+++ public/js/CoffeeScript/hello_world.js
@@ -1,4 +1,15 @@
 (function() {
+  var favorites, name;
+
   console.log("Hello World");
+
+  name = 'me';
+
+  favorites = {
+    food: ["pizza", "pasta"],
+    codingLanguages: ["Sass", "CoffeeScript", "CSS"]
+  };
+
+  console.log(favorites);
 
 }).call(this);
