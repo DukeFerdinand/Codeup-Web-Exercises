2d19ebda6a6ac73bde200fed10701445df3d0607
Douglass Hooker <DouglassHooker@gmail.com>
2017-08-18 10:41:08 -0500

    didn't push much

--- public/php/national_parks.php
+++ public/php/national_parks.php
@@ -3,14 +3,17 @@ require_once '../../db_connect.php';
 header('Access-Control-Allow-Origin: *');
 
 $OFFSET = 0;
+$LIMIT = 0;
 
 if ($_REQUEST && $_REQUEST['OFFSET']){
-
-	
 	$OFFSET = $_REQUEST['OFFSET'];
-}
 
-$stmt = $dbc->query('SELECT id, name, location,date_established,area_in_acres FROM parks LIMIT 4 OFFSET ' . $OFFSET);
+}
+	if ($_REQUEST['LIMIT']){
+		$LIMIT = $_REQUEST['LIMIT'];
+	}
+
+$stmt = $dbc->query('SELECT id, name, location,date_established,area_in_acres FROM parks LIMIT '. $LIMIT . ' OFFSET ' . $OFFSET);
 
 $arr = [];
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
@@ -18,6 +21,7 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 }
 
 print(json_encode($arr));
+// print(json_encode($_REQUEST));
 
 ?>
 
